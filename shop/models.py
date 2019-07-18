6844af4352d7595609fb7820dcc75cc6d6f43b69
Pavel Kozlov <it.pavelkozlov@gmail.com>
2019-07-18 22:31:20 +0300

    Add models. Home page work correctly.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    items = models.ManyToManyField('Item', related_name='articles', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    photo = models.ImageField(upload_to='shop/photos')
+    cathegory = models.ForeignKey('Category', related_name='items', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    name = models.CharField(max_length=150)
+    text = models.TextField()
+    start = models.IntegerField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    is_main = models.BooleanField()
+    main_category = models.ForeignKey('Category', related_name='sub_categories', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Basket(models.Model):
+    sid = models.CharField(max_length=150)
+    items = models.ManyToManyField(Item, through='ItemInBasket')
+
+
+class ItemInBasket(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item, through='ItemInOrder')
+
+
+class ItemInOrder(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    count = models.IntegerField()
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Category, Article
 
 
 # Create your views here.
 def mainpage(request):
     context = dict()
+    context['categories'] = Category.objects.filter(is_main=True)
+    context['articles'] = Article.objects.all()
     return render(request, 'shop/index.html', context=context)
